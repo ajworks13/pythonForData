a953a26c0c9d9b9382af253a41cd89b8688199f0
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2024-07-22 22:30:58 -0400

    trying free data sheets

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 df = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=["A", "B", "C"])
 
+
 print(df)
 print("------------")
 print(df.columns)
@@ -20,3 +21,13 @@ print("------------")
 print(df.max())
 print("------------")
 print(df.head())
+
+
+customers = pd.read_csv("data/customers-100.csv",
+                        index_col=0, parse_dates=True)
+
+
+print(customers.head())
+customers.plot()
+
+plt.show()
