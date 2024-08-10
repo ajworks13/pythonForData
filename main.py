b016ce66c2975907fdc657bc7b95379393003cf0
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2024-08-10 18:34:52 -0400

    Learning from Kaggle

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=["A", "B", "C"])
+# df = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=["A", "B", "C"])
 
 
-print(df)
-print("------------")
-print(df.columns)
-print("------------")
-print(df.info())
-print("------------")
-print(df.describe())
-print("------------")
-print(df["A"].unique())
-print("------------")
-print(df.shape)
-print("------------")
-print(df.size)
-print("------------")
-print(df.max())
-print("------------")
-print(df.head())
+# print(df)
+# print("------------")
+# print(df.columns)
+# print("------------")
+# print(df.info())
+# print("------------")
+# print(df.describe())
+# print("------------")
+# print(df["A"].unique())
+# print("------------")
+# print(df.shape)
+# print("------------")
+# print(df.size)
+# print("------------")
+# print(df.max())
+# print("------------")
+# print(df.head())
 
 
-customers = pd.read_csv("data/customers-100.csv",
-                        index_col=0, parse_dates=True)
+# customers = pd.read_csv("data/customers-100.csv",
+#                         index_col=0, parse_dates=True)
 
 
-print(customers.head())
-customers.plot()
+# print(customers.head())
+# customers.plot()
 
-plt.show()
+# plt.show()
+
+
+data = pd.DataFrame({"Yes": [50, 21], "No": [131, 2]})
+
+print(data)
